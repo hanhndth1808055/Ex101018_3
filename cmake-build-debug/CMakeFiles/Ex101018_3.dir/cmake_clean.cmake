91ec31e116ff0ba3116d7e6ec223567904bf05f7
HanhND <hanhndth1808055@fpt.edu.vn>
2018-10-10 20:28:39 +0700

    So lon nhat be nhat

---
+++ cmake-build-debug/CMakeFiles/Ex101018_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex101018_3.dir/main.c.obj"
+  "Ex101018_3.pdb"
+  "Ex101018_3.exe"
+  "Ex101018_3.exe.manifest"
+  "libEx101018_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex101018_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
