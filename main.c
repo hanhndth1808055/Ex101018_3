8c6e6d3feb2d5c37f331e4d7dc630c01739ecede
HanhND <hanhndth1808055@fpt.edu.vn>
2018-10-10 21:24:59 +0700

    Gia tri lon nho nhat

--- main.c
+++ main.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
 
+int findMaxValue(int *pointer, int sizeOfArray) {
+    int max = *pointer;
+    for (int i = 0; i < sizeOfArray; ++i) {
+        if (max < *(pointer + i)) {
+            max = *(pointer + i);
+        }
+    }
+    return max;
+}
+
+int findMinValue(int *pointer, int sizeOfArray) {
+    int min = *pointer;
+    for (int i = 0; i < sizeOfArray; ++i) {
+        if (min > *(pointer + i)) {
+            min = *(pointer + i);
+        }
+    }
+    return min;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int sizeOfArray;
+    int *p;
+    printf("Moi nhap so luong phan tu tu 1 den 10.\n");
+    scanf("%d", &sizeOfArray);
+    if (sizeOfArray < 1 || sizeOfArray > 10) {
+        printf("So luong phan tu khong dung yeu cau.");
+        return 0;
+    }
+    int array[sizeOfArray];
+    p = array;
+    printf("Moi nhap cac phan tu.\n");
+    for (int i = 0; i < sizeOfArray; ++i) {
+        printf("Moi nhap phan tu thu %d\n", i + 1);
+        scanf("%d", &array[i]);
+    }
+    printf("Hien thi cac phan tu trong mang.\n");
+    for (int i = 0; i < sizeOfArray; ++i) {
+        printf("%d\n", array[i]);
+    }
+    int choice;
+    printf("Moi chon gia tri muon tim 1 | 2\n");
+    printf("1. So lon nhat\n");
+    printf("2. So nho nhat\n");
+    scanf("%d", choice);
+    if (choice == 1) {
+        int ketqua;
+        ketqua = findMaxValue(p, sizeOfArray);
+        printf("So lon nhat la %d\n", ketqua);
+    }
+    if (choice == 2) {
+        int ketqua;
+        ketqua = findMinValue(p, sizeOfArray);
+        printf("So nho nhat la %d\n", ketqua);
+    }
     return 0;
 }
